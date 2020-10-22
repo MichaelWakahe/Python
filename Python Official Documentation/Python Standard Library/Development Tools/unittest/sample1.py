590ffd14e97160bcac3d71c842b35130acc637b3
Michael Wakahe <mikewakahe@gmail.com>
2020-10-22 19:42:16 +0300

    Update to regexp and unittest

---
+++ Python Official Documentation/Python Standard Library/Development Tools/unittest/sample1.py
@@ -0,0 +1,31 @@
+"""
+Run this on the command line as follows:
+    $ python3 sample1.py
+
+Alternatively:
+    $ python3 -m unittest sample1
+
+To increase verbosity, use the '-v' option. The arg works for both cases above.
+"""
+import unittest
+
+
+class TestStringMethods(unittest.TestCase):
+
+    def test_upper(self):
+        self.assertEqual('foo'.upper(), 'FOO')
+
+    def test_isupper(self):
+        self.assertTrue('FOO'.isupper())
+        self.assertFalse('Foo'.isupper())
+
+    def test_split(self):
+        s = 'hello world'
+        self.assertEqual(s.split(), ['hello', 'world'])
+        # check that s.split fails when the separator is not a string
+        with self.assertRaises(TypeError):
+            s.split(2)
+
+
+if __name__ == '__main__':
+    unittest.main()
