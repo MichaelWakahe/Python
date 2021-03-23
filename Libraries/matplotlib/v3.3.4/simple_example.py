33db11180a1a52d034fade1799a0edeaf8a64162
Michael Wakahe <mikewakahe@gmail.com>
2021-03-23 08:36:58 +0300

    Matplotlib examples

---
+++ Libraries/matplotlib/v3.3.4/simple_example.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+plt.show(block=True)
+
+fig, ax = plt.subplots()  # Create a figure containing a single axes.
+#ax.plot([1, 2, 3, 4], [1, 4, 2, 3])  # Plot some data on the axes.
+plt.plot([1, 2, 3, 4], [1, 4, 2, 5])  # Matplotlib plot.
+#plt.show()
+
